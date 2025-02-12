83c2725ade7da051b867a2c9e941b4b579277b08
Kevin Agolla <it@kelima-enterprises.com>
2025-02-12 14:32:02 +0300

    Added simple interest calculator

--- financial_Calculator.py
+++ financial_Calculator.py
@@ -8,8 +8,13 @@ class financialCalc:
     def compound_interest(self):
         A = self.principal * ((1 + self.interest/self.compounded)**(self.duration * self.compounded))
         return round(A, 2)
-        
+    
+    def simple_interest(self):
+        A_S = self.principal * self.interest * self.duration
+        return round(A_S, 2)
 
 Person1 = financialCalc(1000, 0.1, 5)
 Person1.compound_interest()
-print(f"The Amount after {Person1.duration} years with interest rate {Person1.interest*100}% is {Person1.compound_interest()}")
+Person1.simple_interest()
+print(f"The Amount after {Person1.duration} years with compound interest rate {Person1.interest*100}% is {Person1.compound_interest()}")
+print(f"The Amount after {Person1.duration} years with simple interest rate {Person1.interest*100}% is {Person1.simple_interest()}")
