87a407d8028383914d1a1f53d53ebef90fe66872
Kevin Agolla <it@kelima-enterprises.com>
2025-02-12 14:33:49 +0300

    Added simple interest calculator

--- financial_Calculator.py
+++ financial_Calculator.py
@@ -18,3 +18,4 @@ Person1.compound_interest()
 Person1.simple_interest()
 print(f"The Amount after {Person1.duration} years with compound interest rate {Person1.interest*100}% is {Person1.compound_interest()}")
 print(f"The Amount after {Person1.duration} years with simple interest rate {Person1.interest*100}% is {Person1.simple_interest()}")
+
